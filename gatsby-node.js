4139dc6aac1e713885b9991bc17d0840c6336f6d
abhiaiyer91 <abhiaiyer91@gmail.com>
2019-08-22 13:47:24 -0700

    staging fixes

--- gatsby-node.js
+++ gatsby-node.js
@@ -2,6 +2,9 @@ const Promise = require('bluebird')
 const path = require('path')
 
 exports.createPages = ({ graphql, actions }) => {
+
+  console.log(GATSBY_TELEMETRY_API)
+
   const { createPage } = actions
 
   return new Promise((resolve, reject) => {
